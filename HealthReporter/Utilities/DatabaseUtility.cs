38ac3e4347b6bdc94db7a35e118eff9210ca6fc1
Sander <sander.sats@gmail.com>
2016-09-18 21:11:17 +0300

    Alternative folder structure idea with basic database support

---
+++ HealthReporter/Utilities/DatabaseUtility.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Insight.Database;
+using System.Data.SQLite;
+using HealthReporter.Models;
+using System.IO;
+
+namespace HealthReporter.Utilities
+{
+    class DatabaseUtility
+    {
+        static String dbName = "HealthReporter.sqlite";
+
+        static public SQLiteConnectionStringBuilder getConnectionStringBuilder()
+        {
+            return new SQLiteConnectionStringBuilder() { DataSource = dbName };
+        }
+
+        static public void checkDb()
+        {
+            if (!File.Exists(dbName)) {
+                createDatabase();
+            }
+        }
+
+        private static void createDatabase()
+        {
+            SQLiteConnection dbConnection;
+            SQLiteConnectionStringBuilder builder;
+            SQLiteConnection.CreateFile(dbName);
+            builder = getConnectionStringBuilder();
+            dbConnection = new SQLiteConnection(builder.ConnectionString);
+
+            dbConnection.ExecuteSql(ClientRepository.getInsertSql());
+        }
+
+        static public SQLiteConnection getConnection()
+        {
+            var connection = new SQLiteConnection(getConnectionStringBuilder().ConnectionString).OpenConnection();
+
+            return connection;
+        }
+    }
+}
---
+++ HealthReporterUnitTests/ViewModels/MainWindowViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HealthReporter;
+
+// https://msdn.microsoft.com/en-us/library/ms182532.aspx
+
+namespace HealthReporterUnitTests
+{
+    [TestClass]
+    public class MainWindowViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+            double a = 11.99;
+            double b = 12;
+            double expectedSum = 25.55;
+
+            // act
+            double actualSum = a + b;
+
+            // assert
+            Assert.AreEqual(expectedSum, actualSum, 0.001, "Wrong sum");
+        }
+    }
+}
