d407cf0659d01bfbb3321bd4627527926d9803a6
Sander <sander.sats@gmail.com>
2016-10-09 18:20:10 +0300

    closes #53 Added add categories view

---
+++ HealthReporter/Controls/AddNewTestCategoryControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿using HealthReporter.Models;
+using HealthReporter.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HealthReporter.Controls
+{
+    /// <summary>
+    /// Interaction logic for AddNewTestCategoryControl.xaml
+    /// </summary>
+    public partial class AddNewTestCategoryControl : UserControl
+    {
+        public MainWindow _parent;
+
+        public AddNewTestCategoryControl(MainWindow parentWindow, byte[] parentCategory)
+        {
+            InitializeComponent();
+            this._parent = parentWindow;
+
+            var repo = new TestCategoryRepository();
+
+            IList<TestCategory> tests = repo.FindRootCategories();
+
+            if (tests.Count() > 0)
+            {
+                tests.Add(new TestCategory());
+            }
+
+            parentSelector.ItemsSource = tests;
+            if (parentCategory != null)
+            {
+                int index = Helpers<TestCategory>.GetIndexById(tests, parentCategory);
+                if (index >= 0)
+                {
+                    parentSelector.SelectedIndex = index;
+                }
+            }
+        }
+
+        private void btn_Back(object sender, RoutedEventArgs e)
+        {
+            this._parent.stkTest.Children.Clear();
+            TestsUserControl obj = new TestsUserControl(this._parent);
+            this._parent.stkTest.Children.Add(obj);
+        }
+
+        private void btn_CreateNewTestCategory(object sender, RoutedEventArgs e)
+        {
+            //TODO: add validation also.
+            byte[] parentId = null;
+            var parentCategory = (TestCategory)parentSelector.SelectedItem;
+            if (parentCategory != null)
+            {
+                parentId = parentCategory.id;
+            }
+            var testCategory = new TestCategory() { name = this.name.Text, parentId = parentId };
+
+            var repo = new TestCategoryRepository();
+            repo.Insert(testCategory);
+
+            this._parent.stkTest.Children.Clear();
+            TestsUserControl obj = new TestsUserControl(this._parent);
+            this._parent.stkTest.Children.Add(obj);
+
+        }
+    }
+}
---
+++ HealthReporter/Utilities/Helpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthReporter.Models;
+
+namespace HealthReporter.Utilities
+{
+    public static class Helpers<T>
+    {
+        // finds index by id in an IList. Return -1 if id not found
+        public static int GetIndexById(IList<T> list, byte[] id)
+        {
+            if (id == null)
+            {
+                return -1;
+            }
+
+            var length = list.Count();
+            for (int i = 0; i < length; i++)
+            {
+                var elemId = ((IHasPrimaryKey)list[i]).GetPrimaryKey();
+                if (elemId.SequenceEqual(id))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+    public interface IHasPrimaryKey
+    {
+        byte[] GetPrimaryKey();
+    }
+}
