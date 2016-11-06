729c6f1c8be275792407497dbeedd17045cfd827
Sander <sander.sats@gmail.com>
2016-11-06 19:15:12 +0200

    Added calculator test

--- HealthReporterUITests/Program.cs
+++ HealthReporterUITests/Program.cs
@@ -18,17 +18,34 @@ namespace HealthReporterUITests
 
             var winFinder = By.Name("Calculator").AndType(ControlType.Window);
             var win = Winium.Cruciatus.CruciatusFactory.Root.FindElement(winFinder);
+
+            var elements = win.FindElements(winFinder);
+            Console.WriteLine("WTF");
+            Console.WriteLine(elements);
+            foreach (var element in elements)
+            {
+                Console.WriteLine("element");
+                Console.WriteLine(element.ToString());
+            }
+
             var menu = win.FindElementByUid("MenuBar").ToMenu();
 
             menu.SelectItem("View$Scientific");
             menu.SelectItem("View$History");
 
             win.FindElementByUid("132").Click(); // 2
+            var elements2 = win.FindElementByUid("150");
+            Console.WriteLine(elements2.GetAutomationPropertyValue<String>(AutomationElement.NameProperty));
             win.FindElementByUid("93").Click(); // +
+            Console.WriteLine(elements2.GetAutomationPropertyValue<String>(AutomationElement.NameProperty));
             win.FindElementByUid("134").Click(); // 4
+            Console.WriteLine(elements2.GetAutomationPropertyValue<String>(AutomationElement.NameProperty));
             win.FindElementByUid("97").Click(); // ^
+            Console.WriteLine(elements2.GetAutomationPropertyValue<String>(AutomationElement.NameProperty));
             win.FindElementByUid("138").Click(); // 8
+            Console.WriteLine(elements2.GetAutomationPropertyValue<String>(AutomationElement.NameProperty));
             win.FindElementByUid("121").Click(); // =
+            Console.WriteLine(elements2.GetAutomationPropertyValue<String>(AutomationElement.NameProperty));
 
             calc.Close();
         }
--- HealthReporterUnitTests/ViewModels/MainWindowViewModelTests.cs
+++ HealthReporterUnitTests/ViewModels/MainWindowViewModelTests.cs
@@ -2,6 +2,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HealthReporter;
 
+using System.Windows.Automation;
+using Winium.Cruciatus.Core;
+using Winium.Cruciatus.Extensions;
+
 // https://msdn.microsoft.com/en-us/library/ms182532.aspx
 
 namespace HealthReporterUnitTests
@@ -23,5 +27,37 @@ namespace HealthReporterUnitTests
             // assert
             Assert.AreEqual(expectedSum, actualSum, 0.001, "Wrong sum");
         }
+
+        [TestMethod]
+        public void TestCalc()
+        {
+            var calc = new Winium.Cruciatus.Application("C:/windows/system32/calc.exe");
+            calc.Start();
+
+            var winFinder = By.Name("Calculator").AndType(ControlType.Window);
+            var win = Winium.Cruciatus.CruciatusFactory.Root.FindElement(winFinder);
+            var menu = win.FindElementByUid("MenuBar").ToMenu();
+
+            menu.SelectItem("View$Scientific");
+            menu.SelectItem("View$History");
+
+            double expectedSum = 2 + Math.Pow(4, 8);
+
+            win.FindElementByUid("132").Click(); // 2
+            win.FindElementByUid("93").Click(); // +
+            win.FindElementByUid("134").Click(); // 4
+            win.FindElementByUid("97").Click(); // ^
+            win.FindElementByUid("138").Click(); // 8
+            win.FindElementByUid("121").Click(); // =
+
+            var resultBox = win.FindElementByUid("150");
+            double actualSum = Double.Parse(resultBox.GetAutomationPropertyValue<String>(AutomationElement.NameProperty));
+
+            calc.Close();
+            Assert.AreEqual(expectedSum, actualSum, 0.001, "Wrong sum");
+        }
+
+        //[TestMethod]
+        //public
     }
 }
